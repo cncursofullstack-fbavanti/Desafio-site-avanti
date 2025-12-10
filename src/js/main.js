16163ce509089df50b9a0c91d544190d8a0ebe34
Caetano Neto <caetano.n@gmail.com>
2025-12-10 14:23:36 -0300

    atualizando menu, header e capacitações

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 // src/js/main.js
+import { createStickyHeader } from "./components/stickyHeader.js";
 import { createFullscreenHeader } from "./components/fullscreenHeader.js";
 import { createCallout01 } from "./components/callout01.js";
 import { createCallout02 } from "./components/callout02.js";
@@ -7,10 +8,32 @@ import { createTestimonial } from "./components/testimonial.js";
 import { createMarketing } from "./components/marketing.js";
 import { createFooter } from "./components/footer.js";
 
+let initialized = false;
+
+function observeScrollForHeader() {
+  const header = document.querySelector('#stickyHeader');
+  console.log(header)
+
+  function handleScroll() {
+    if (window.scrollY > 80) {
+      header.classList.add('BG');
+    } else {
+      header.classList.remove('BG');
+    }
+  }
+
+  // Adiciona o listener de scroll
+  window.addEventListener('scroll', handleScroll);
+  
+  // Checa a posição inicial (caso a página já esteja rolada)
+  handleScroll();
+}
+
 function initApp() {
   const root = document.getElementById("app-root");
   if (!root) return;
-
+  
+  root.appendChild(createStickyHeader());
   const header = createFullscreenHeader();
   
   const main = document.createElement("main");
@@ -26,6 +49,13 @@ function initApp() {
   root.appendChild(header);
   root.appendChild(main);
   root.appendChild(footer);
+
+  // Chama a função quando o DOM estiver pronto
+  observeScrollForHeader();
 }
 
 document.addEventListener("DOMContentLoaded", initApp);
+
+
+
+
